be72905822e06db01cb6c5199d22bc2506055546
seo <syh05247@naver.com>
2022-11-11 18:08:02 +0900

    카테고리 작업 중

---
+++ src/main/java/org/zerock/b01/controller/BoardController.java
@@ -0,0 +1,33 @@
+package org.zerock.b01.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.b01.dto.BoardDTO;
+import org.zerock.b01.dto.PageRequestDTO;
+import org.zerock.b01.dto.PageResponseDTO;
+import org.zerock.b01.service.BoardService;
+
+@Controller
+@RequestMapping("/board")
+@Log4j2
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+
+        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+
+        log.info(responseDTO);
+
+        model.addAttribute("responseDTO", responseDTO);
+
+    }
+
+}
---
+++ src/test/java/org/zerock/b01/B01ApplicationTests.java
@@ -0,0 +1,14 @@
+package org.zerock.b01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootTest
+class B01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
