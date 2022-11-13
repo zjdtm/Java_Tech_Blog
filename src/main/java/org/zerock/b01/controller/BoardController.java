c5215b4fe8d3afc00ad0aaa3a6b459bb8f13599c
seo <syh05247@naver.com>
2022-11-13 13:03:00 +0900

    quill 에디터를 사용한 글쓰기 등록 (codeblock, image (base64)처리 수정 중..)

--- src/main/java/org/zerock/b01/controller/BoardController.java
+++ src/main/java/org/zerock/b01/controller/BoardController.java
@@ -63,7 +63,7 @@ public class BoardController {
 
     }
 
-    @GetMapping("/read")
+    @GetMapping({"/read", "/modify"})
     public void read( Long bno, PageRequestDTO pageRequestDTO, Model model){
 
         BoardDTO boardDTO = boardService.readOne(bno);
@@ -71,6 +71,46 @@ public class BoardController {
         log.info(boardDTO);
 
         model.addAttribute("dto", boardDTO);
+
+    }
+
+    @PostMapping("/modify")
+    public String modify(PageRequestDTO pageRequestDTO, @Valid BoardDTO boardDTO, BindingResult bindingResult,
+                         RedirectAttributes redirectAttributes){
+
+        log.info("board modify post........" + boardDTO);
+
+        if(bindingResult.hasErrors()){
+            log.info("has errors");
+
+            String link = pageRequestDTO.getLink();
+
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            redirectAttributes.addAttribute("bno", boardDTO.getBno());
+
+            return "redirect:/board/modify?"+link;
+        }
+
+        boardService.modify(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", "modified");
+
+        redirectAttributes.addAttribute("bno", boardDTO.getBno());
+
+        return "redirect:/board/read";
+
+    }
+
+    @PostMapping("/remove")
+    public String remove(Long bno, RedirectAttributes redirectAttributes){
+
+        log.info("remove post.. " + bno);
+
+        boardService.remove(bno);
+
+        redirectAttributes.addFlashAttribute("result", "removed");
+
+        return "redirect:/board/list";
 
     }
 
