1ad31b4aa2d87f96c28648cb54dc480da5ba6852
seo <syh05247@naver.com>
2023-03-09 22:20:29 +0900

    Feat: MemberService 추가
    Member 등록, 중복회원 조회, 회원 찾기 비즈니스 로직

--- src/main/java/org/zerock/b01/service/MemberService.java
+++ src/main/java/org/zerock/b01/service/MemberService.java
@@ -1,13 +1,36 @@
 package org.zerock.b01.service;
 
-import org.zerock.b01.dto.MemberJoinDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.b01.domain.Member;
+import org.zerock.b01.repository.MemberRepository;
 
-public interface MemberService {
+import java.util.List;
 
-    static class MidExistException extends Exception{
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
 
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
     }
 
-    void join(MemberJoinDTO memberJoinDTO)throws MidExistException;
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByNickname(member.getNickname());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    public Member findOne(Long memberId){
+        return memberRepository.findById(memberId).orElseThrow();
+    }
 
 }
