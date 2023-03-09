cdc6a68ede5e8da9264ce9ddc8f46522d66bdf14
seo <syh05247@naver.com>
2023-03-09 22:21:48 +0900

    Test: MemberServiceTest 추가
    Member CRUD 테스트 확인 Delete 는 X

---
+++ src/test/java/org/zerock/b01/service/MemberServiceTest.java
@@ -0,0 +1,73 @@
+package org.zerock.b01.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.b01.domain.Member;
+import org.zerock.b01.repository.MemberRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void member_register() throws Exception{
+
+        // given
+        Member member = new Member("겐지", "겐지@naver.com", "겐지123");
+
+        // when
+        Long savedId = memberService.join(member);
+
+        // then
+        assertEquals(member, memberService.findOne(savedId));
+    }
+
+    @Test
+    public void member_duplicate() throws Exception {
+
+        // given
+        Member member1 = new Member("윈스턴", "윈스턴@naver.com", "윈스턴123");
+        Member member2 = new Member("윈스턴", "윈스턴@naver.com", "윈스턴123");
+
+        // when
+        memberService.join(member1);
+
+        // then
+        IllegalStateException thrown = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertEquals("이미 존재하는 회원입니다.", thrown.getMessage());
+
+    }
+
+    @Test
+    public void member_change() throws Exception {
+
+        // given
+        Member member = new Member("윈스턴", "윈스턴@naver.com", "윈스턴123");
+
+        // when
+        Long savedId = memberService.join(member);
+        Member findMember = memberService.findOne(savedId);
+
+        findMember.change("메이", "메이@naver.com", "메이123");
+
+        // then
+        Member updatedMember = memberService.findOne(savedId);
+        assertEquals(updatedMember.getNickname(), "메이");
+        assertEquals(updatedMember.getEmail(), "메이@naver.com");
+        assertEquals(updatedMember.getPassword(), "메이123");
+
+
+    }
+
+}
