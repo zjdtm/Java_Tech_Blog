b5476d36ee962320a910a28c054b726c3fb5998c
seo <syh05247@naver.com>
2023-03-09 22:26:10 +0900

    Test: BoardServiceTests 추가
    Board CRUD Test 추가 Delete x

--- src/test/java/org/zerock/b01/service/BoardServiceTests.java
+++ src/test/java/org/zerock/b01/service/BoardServiceTests.java
@@ -4,148 +4,97 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.zerock.b01.domain.BoardImage;
-import org.zerock.b01.dto.*;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.b01.domain.Board;
+import org.zerock.b01.domain.Member;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 @Log4j2
 public class BoardServiceTests {
 
     @Autowired
-    private BoardService boardService;
+    BoardService boardService;
 
     @Test
-    public void testRegister(){
+    public void board_register() {
+        //given
+        Member member = new Member("lofi", "lofi@naver.com", "11111");
 
-        log.info(boardService.getClass().getName());
+        Board board = new Board("패스트 캠퍼스 회고록", "안녕하세요 저는 ....", member);
 
-        BoardDTO boardDTO = BoardDTO.builder()
-                .title("Sample Title...")
-                .content("Sample Content...")
-                .writer("user00")
-                .build();
+        //when
+        Long register_id = boardService.register(board);
 
-        Long bno = boardService.register(boardDTO);
+        //then
+        assertThat(board).isEqualTo(boardService.findOne(register_id));
+    }
 
-        log.info("bno: " + bno);
+    @Test
+    public void board_findAll(){
+
+        // given
+        Member member = new Member("lofi", "lofi@naver.com", "11111");
+
+        Board board1 = new Board("요즘 경제가 어려워요", "요즘 참 살기 힘들시죠.......", member);
+        Board board2 = new Board("코딩이 너무 어려워요 어떡하죠?", "AI의 발전으로 코딩의 대한 미래는 .....", member);
+
+        // when
+        boardService.register(board1);
+        boardService.register(board2);
+
+        List<Board> boards = boardService.findBoards();
+
+        // then
+        assertThat(boards.get(0).getTitle()).isEqualTo(board1.getTitle());
+        assertThat(boards.get(1).getTitle()).isEqualTo(board2.getTitle());
+    }
+
+    @Test
+    public void board_update() throws Exception {
+
+        // given
+        Member member = new Member("lofi", "lofi@naver.com", "11111");
+
+        Board board = new Board("백엔드 개발자가 알아야할 필수 지식", "백엔드 개발자라면 꼭 알아야 할 .....", member);
+
+        // when
+        Long registeredId = boardService.register(board);
+        Board findBoard = boardService.findOne(registeredId);
+
+        findBoard.change("프론트엔드 개발자가 알아야할 필수 지식", "프론트 엔드 개발자가 꼭 알아야 할 .....");
+
+        // then
+        Board updatedBoard = boardService.findOne(registeredId);
+        assertThat(updatedBoard.getTitle()).isEqualTo("프론트엔드 개발자가 알아야할 필수 지식");
+        assertThat(updatedBoard.getContent()).isEqualTo("프론트 엔드 개발자가 꼭 알아야 할 .....");
+        assertThat(updatedBoard.getMember().getEmail()).isEqualTo("lofi@naver.com");
 
     }
 
     @Test
-    public void testReadOne() {
+    public void board_like() {
 
-        BoardDTO boardDTO = boardService.readOne(100L);
+        // given
+        Member member = new Member("lofi", "lofi@naver.com", "11111");
 
-        log.info(boardDTO);
+        Board board = new Board("백엔드 개발자가 알아야할 필수 지식", "백엔드 개발자라면 꼭 알아야 할 .....", member);
 
-    }
+        // when
+        Long registeredId = boardService.register(board);
+        Board findBoard = boardService.findOne(registeredId);
 
-    @Test
-    public void testModify(){
+        findBoard.boardLike();
+        findBoard.boardLike();
+        findBoard.boardNotLike();
 
-        BoardDTO boardDTO = BoardDTO.builder()
-                .bno(101L)
-                .title("Updated....101")
-                .content("Updated content 101...")
-                .build();
-
-        boardDTO.setFileNames(Arrays.asList(UUID.randomUUID()+"_zzz.jpg"));
-
-        boardService.modify(boardDTO);
+        // then
+        assertThat(findBoard.getLike()).isEqualTo(1);
 
     }
 
-    @Test
-    public void testList() {
-
-        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
-                .type("tcw")
-                .keyword("1")
-                .page(1)
-                .size(10)
-                .build();
-
-        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
-
-        log.info(responseDTO);
-
-    }
-
-    @Test
-    public void testRegisterWithImages(){
-
-        log.info(boardService.getClass().getName());
-
-        BoardDTO boardDTO = BoardDTO.builder()
-                .title("File...Sample Title...")
-                .content("Sample Content...")
-                .writer("user00")
-                .build();
-
-        boardDTO.setFileNames(
-                Arrays.asList(
-                        UUID.randomUUID()+"_aaa.jpg",
-                        UUID.randomUUID()+"_bbb.jpg",
-                        UUID.randomUUID()+"_bbb.jpg"
-                ));
-
-        Long bno = boardService.register(boardDTO);
-
-        log.info("bno: " + bno);
-
-    }
-
-    @Test
-    public void testReadAll(){
-
-        Long bno = 101L;
-
-        BoardDTO boardDTO = boardService.readOne(bno);
-
-        log.info(boardDTO);
-
-        for(String fileName : boardDTO.getFileNames()){
-
-            log.info(fileName);
-
-        }
-    }
-
-    @Test
-    public void testRemoveAll(){
-
-        Long bno = 1L;
-
-        boardService.remove(bno);
-
-    }
-
-    @Test
-    public void testListWithAll(){
-
-        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
-                .page(1)
-                .size(10)
-                .build();
-
-        PageResponseDTO<BoardListAllDTO> responseDTO = boardService.listWithAll(pageRequestDTO);
-
-        List<BoardListAllDTO> dtoList = responseDTO.getDtoList();
-
-        dtoList.forEach(boardListAllDTO -> {
-
-            log.info(boardListAllDTO.getBno() + ":" + boardListAllDTO.getTitle());
-
-            if(boardListAllDTO.getBoardImages() != null){
-                for(BoardImageDTO boardImage : boardListAllDTO.getBoardImages()){
-                    log.info(boardImage);
-                }
-            }
-            log.info("------------------------------");
-        });
-    }
 }
