74738395cc75f76d0ee273a747e70808eb73d1e5
seo <syh05247@naver.com>
2023-03-09 22:22:27 +0900

    Feat: MemberRepository 추가

--- src/main/java/org/zerock/b01/repository/MemberRepository.java
+++ src/main/java/org/zerock/b01/repository/MemberRepository.java
@@ -1,26 +1,12 @@
 package org.zerock.b01.repository;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 import org.zerock.b01.domain.Member;
 
-import java.util.Optional;
+import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, String> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    @EntityGraph(attributePaths = "roleSet")
-    @Query("select m from Member m where m.mid = :mid and m.social = false")
-    Optional<Member> getWithRoles(@Param("mid") String mid);
+    List<Member> findByNickname(String nickname);
 
-    @EntityGraph(attributePaths = "roleSet")
-    Optional<Member> findByEmail(String email);
-
-    @Modifying
-    @Transactional
-    @Query("update Member m set m.mpw =:mpw where m.mid = :mid")
-    void updatePassword(@Param("mpw") String password, @Param("mid")String mid);
 }
