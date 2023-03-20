5002eee5b53ac3465f98dcb468990c8e6b4449a1
seo <syh05247@naver.com>
2023-03-20 23:00:08 +0900

    Style: Controller, DTO 변경
    URI 변경 DTO @Valid 를 적용시키기 위한 validation 추가

--- src/main/java/org/zerock/b01/controller/BoardController.java
+++ src/main/java/org/zerock/b01/controller/BoardController.java
@@ -1,22 +1,55 @@
 package org.zerock.b01.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.b01.dto.*;
 import org.zerock.b01.service.BoardService;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
+@Slf4j
 public class BoardController {
 
     private final BoardService boardService;
 
-    @GetMapping
-    public String BoardList(Model model, Pageable pageable){
-        return "board/list";
+    @GetMapping("/")
+    public String BoardList(PageRequestDTO pageRequestDTO, Model model){
+
+        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+
+        log.info(String.valueOf(responseDTO));
+
+        model.addAttribute("responseDTO", responseDTO);
+
+        return "/list";
+
     }
 
+    @GetMapping("/write")
+    public String getWrite(Model model) {
+        model.addAttribute("boardForm", new BoardDTO());
+        return "/write";
+    }
+
+    @PostMapping("/write")
+    public String registerBoard(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+
+        if(bindingResult.hasErrors()){
+
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+
+            return "redirect:/write";
+        }
+
+        boardService.register(boardDTO);
+
+        return "redirect:";
+    }
 
 }
--- src/main/java/org/zerock/b01/dto/BoardDTO.java
+++ src/main/java/org/zerock/b01/dto/BoardDTO.java
@@ -1,5 +1,7 @@
 package org.zerock.b01.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,13 +17,15 @@ public class BoardDTO {
 
     private Long bno;
 
+    @NotEmpty(message = "제목은 최소 3글자 이상이어야 합니다.")
+    @Size(min = 3, max = 100)
     private String title;
 
+    @NotEmpty(message = "내용은 비어있을 수 없습니다.")
     private String content;
 
+    @NotEmpty
     private String writer;
-
-//    private int like;
 
     private LocalDateTime regDate;
 
