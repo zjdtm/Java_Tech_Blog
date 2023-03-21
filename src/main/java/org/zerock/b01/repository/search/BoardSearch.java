d9e3a7560943ce4a712a77c6e5a4fa3f364819f7
seoyonghyun <syh05247@naver.com>
2023-03-21 21:19:40 +0900

    Feat : BoardSearch 및 상속 추가
    QueryDsl 을 이용하여 게시판의 검색 기능을 추가하기 위한 BoardSearch 인터페이스 추가

--- src/main/java/org/zerock/b01/repository/search/BoardSearch.java
+++ src/main/java/org/zerock/b01/repository/search/BoardSearch.java
@@ -2,9 +2,12 @@ package org.zerock.b01.repository.search;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.zerock.b01.domain.Board;
 
 public interface BoardSearch {
 
     Page<Board> searchAll(String keyword, Pageable pageable);
+
+//    Slice<Board> searchSlice(Long bno, String keyword, Pageable pageable);
 }
