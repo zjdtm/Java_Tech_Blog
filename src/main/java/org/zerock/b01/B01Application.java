5da6508b22faa5d93ad5194ca8f7b5051eb534ef
seoyonghyun <syh05247@naver.com>
2023-04-02 22:50:21 +0900

    Feat: QueyDsl 환경을 위한 bean 추가
    jpaQueryFactory bean 설정을 위한 코드 추가

--- src/main/java/org/zerock/b01/B01Application.java
+++ src/main/java/org/zerock/b01/B01Application.java
@@ -1,7 +1,10 @@
 package org.zerock.b01;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -10,6 +13,11 @@ public class B01Application {
 
     public static void main(String[] args) {
         SpringApplication.run(B01Application.class, args);
+    }
+
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager em){
+        return new JPAQueryFactory(em);
     }
 
 }
